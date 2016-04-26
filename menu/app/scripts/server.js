2da4e1373505f07052756d6ebd00b9826cfc52ac
lessnica <lessenka2>
2016-04-26 19:53:45 +0300

    26.04 server

--- menu/app/scripts/server.js
+++ menu/app/scripts/server.js
@@ -2,14 +2,77 @@ var http = require ('http');
 var fs = require ('fs');
 
 http.createServer(function(req,res) {
-  if(req.method === 'GET') {
+  console.log('server created');
+  if (req.method === 'GET') {
+
+    if (req.url.includes('html')) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.readFile('../../dist/'+req.url, function (err, data) {
+        res.end(data);
+      });
+    };
+
     if(req.url.includes('json')) {
+      console.log('json request');
       res.writeHead(200,{'Content-type':'application/json'});
-      fs.readfile(req.url, function(err,data) {
+      fs.readFile(req.url.replace('/',''), function(err,data) {
+        console.log(req.url.replace('/',''), data);
         res.end(data);
       });
     }
 
+
+    if (req.url.includes('css')) {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      fs.readFile('../../dist'+req.url, function (err, data) {
+        console.log(req.url);
+        res.end(data);
+      });
+    };
+
+    if (req.url.includes('js') && !req.url.includes('json')) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      fs.readFile('../../dist'+req.url, function (err, data) {
+        res.end(data);
+      });
+    };
+
+    if (req.url.includes('favicon')) {
+      res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+      res.end();
+    };
+
+    if (req.url.includes('.jpg')) {
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      fs.readFile(req.url.replace('/', ''), function (err, data) {
+        res.end(data);
+      });
+    };
+
+    if (req.url.includes('/fetch')) {
+      console.log(fetch);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      fs.readFile('todolist.json', function (err, data) {
+        res.end(data);
+      });
+    }
+
+
   }
-  console.log('server running 8080');
+
+  if (req.method === 'POST') {
+    var jasonString = '';
+    req.on('data',function(data) {
+      jasonString +=data;
+    });
+    req.on('end', function() {
+      fs.writeFile('todolist.json',jasonString, function() {
+        res.end();
+      });
+    });
+  }
+
+
 }).listen(8080);
+
+console.log('server running 8080');
